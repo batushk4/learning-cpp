6de9b27054cdb39ef160d657d8483a4be6c00247
barfly <oseias.laborda@gmail.com>
2023-02-23 20:15:55 -0400

    anatomia de um código c++

---
+++ 1.fundamentos/02/anatomia.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+int main() {
+
+    // How is a book organized?
+
+    // A code block is a grouping of one or more statements.
+    {
+        ; // a statement is a syntyctic unit of an imperative programming language
+        ;
+    }
+
+    std::cout
+        <<
+        "Hi there!\n"
+        <<
+        "bye"
+        << std::endl;
+
+    std::cout << "Sorte que você é linda!\n";
+    return 0;
+}
